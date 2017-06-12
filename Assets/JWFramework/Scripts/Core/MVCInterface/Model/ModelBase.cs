cf78639f2a829f8228ae8415b326966bea6454a2
jccg891113 <sachuan@foxmail.com>
2017-06-12 18:46:48 +0800

    tmp
    mvc开始设计架构

---
+++ Assets/JWFramework/Scripts/Core/MVCInterface/Model/ModelBase.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JWFramework.MVC
+{
+	public class ModelBase
+	{
+		protected void PostNotification (string modelName)
+		{
+		}
+	}
+}
---
+++ Assets/JWFramework/Scripts/Core/MVCInterface/View/JWViewBase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace JWFramework.MVC
+{
+	public abstract class JWViewBase : MonoBehaviour
+	{
+		public void Awake ()
+		{
+		}
+
+		protected abstract string[] GetNotificationName ();
+	}
+}
